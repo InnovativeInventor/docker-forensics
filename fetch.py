468fa6c09abe2aa93aab61e68e87433abe32235c
Max Fan <git@max.fan>
2022-08-23 17:37:26 -0500

    Add support for reversing locally built images

--- fetch.py
+++ fetch.py
@@ -3,14 +3,21 @@ import typer
 import os
 import tqdm
 
-def main(repository: str):
+from typing import Optional
+
+def main(repository: Optional[str] = None):
     client = docker.from_env()
-    client.images.pull(repository, all_tags=True)
+
+    if repository:
+        client.images.pull(repository, all_tags=True)
+        images = client.images.list(repository, all=True)
+    else:
+        images = client.images.list(all=True)
 
     if not os.path.isdir("images"):
         os.mkdir("images")
 
-    for image in tqdm.tqdm(client.images.list(repository, all=True)):
+    for image in tqdm.tqdm(images):
         fname = "images/" + "-".join([x.split(":")[1] for x in image.tags]) + "-" + image.short_id.split(":")[1]
         if not os.path.isdir(fname):
             os.mkdir(fname)
